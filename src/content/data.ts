18afb595afb7d9e40ebb81e5494b2e624e56d61e
AlifAkbar307 <137366629+AlifAkbar307@users.noreply.github.com>
2026-08-05 15:25:49 +0700

    spasi

--- src/content/data.ts
+++ src/content/data.ts
@@ -640,7 +640,7 @@ FYI harga berikut merupakan LMR hari esok untuk menampilkan estimasi harga FedEx
         title: "Ask for Approval – LMR Not Available (Extra Charge Domestik)",
         fields: [],
         body: `kak **XXX** berikut harga dari FedEx. Quotes tersedia di Drive. Customer terkena extra charge pengantaran ke XXX sebesar XXX IDR/KG. Mohon Approvalnya.
-        FYI harga berikut merupakan LMR hari esok untuk menampilkan estimasi harga FedEx, karena untuk harga FedEx pada hari pickup belum tersedia`,
+FYI harga berikut merupakan LMR hari esok untuk menampilkan estimasi harga FedEx, karena untuk harga FedEx pada hari pickup belum tersedia`,
       },
       {
         id: "newest-proposal",
