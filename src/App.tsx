38f02c16f58889d724be234bd23873998abd604f
AlifAkbar307 <137366629+AlifAkbar307@users.noreply.github.com>
2026-09-17 17:14:44 +0700

    tes construction undo

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import { ToolPage } from './pages/ToolPage';
 import { EligibilityChecker } from './pages/EligibilityChecker';
 import { QuoteParser } from './pages/QuoteParser';
 import { JiraHelper } from './pages/JiraHelper';
+import { ContohDokumen } from './pages/ContohDokumen';
 import { CiplVsSkp } from './pages/CiplVsSkp';
 import { PickupCall } from './pages/PickupCall';
 import { navItems } from './content/data';
